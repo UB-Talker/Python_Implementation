e7eda80092513af00a7531c8719a974744861ff1
Corey Ropell <coreyrop@buffalo.edu>
2018-11-01 16:43:15 -0400

    Made gTTS talk through pygame mixer, has a skipping issue
    gTTS now speaks through the pygame mixer so should work cross-platform. There is an issue that the voice seems to sort of skip when it is starting to speak.

--- TTS/TTS.py
+++ TTS/TTS.py
@@ -1,27 +1,21 @@
 from gtts import gTTS
-import os
-from sys import platform
 import pyttsx3
-
+from pygame import mixer
+from io import BytesIO
 
 engine = pyttsx3.init()
-if platform == 'linux' or platform == 'linux2':
-    pass
-
-elif platform == 'darwin':
-    media_cmd = 'afplay '
-
-elif platform == 'win32':
-    media_cmd = 'start '
+mixer.init()
 
 
 def speak(text, lang='en'):
+
     try:
         tts = gTTS(text=text, lang=lang)
-        sf = 'temp.mp3'
-        tts.save(sf)
-        os.system(media_cmd + sf)
-        os.remove(sf)
+        sf = BytesIO()
+        tts.write_to_fp(sf)
+        sf.seek(0)
+        mixer.music.load(sf)
+        mixer.music.play()
     except:
         engine.say(text)
         engine.runAndWait()
