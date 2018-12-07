afbea31c3b19b216d83b5f868997758c10911fc0
sam21marchant <sam17marchant@gmail.com>
2018-12-07 15:16:48 -0500

    Changed tts to pyttsx3

--- TTS/TTS.py
+++ TTS/TTS.py
@@ -1,30 +1,15 @@
-from gtts import gTTS
-from gtts.tts import gTTSError
-from ttslib import tts as local_tts
-from pygame import mixer
-from io import BytesIO
-import socket
+import pyttsx3
+import platform
 
 
-def internet_connected():
-    try:
-        socket.gethostbyname('www.google.com')
-        return True
-    except socket.gaierror:
-        return False
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 
-def speak(text, lang='en'):
-    try:
-        gtts = gTTS(text=text, lang=lang)
-        sf = BytesIO()
-        gtts.write_to_fp(sf)
-        sf.seek(0)
-        mixer.music.load(sf)
-        mixer.music.play()
-    except gTTSError:
-        local_tts(text, 'en')
-    pass
-
-
-mixer.init()
+engine = pyttsx3.init()
+if platform.system() == "Windows":
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id) #Set voice to default male voice in sapi5
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate - 25) #slow the speed of the voice to sound more natural
